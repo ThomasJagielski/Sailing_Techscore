f098d820f82b0c3ffdf9f7f9017c57746c05be87
thomasjagielski <tjagielski@olin.edu>
2018-12-03 18:23:41 -0500

    first commit

---
+++ Documents/Code/Sailing_Techscore/scoring_system.py
@@ -0,0 +1,49 @@
+from pandas import *
+import numpy as np
+
+# Test Link: https://scores.collegesailing.org/f18/victorian-urn/
+
+#link = input('Link to Results = ')
+#event = input("Event Type (SC, SC_alt, Coed A, Coed B, Coed C, W SC, Women's A, Women's B)= ")
+
+event = 'Coed A'
+
+#link = input('Regatta Results Link = ')
+
+#table = pandas.read_html(link, header=0, index_col=0)
+table = pandas.read_html('https://scores.collegesailing.org/f18/victorian-urn/', header=0, index_col=0)
+
+table2 = table[0]
+"""table2.columns = ['Position', 'Flag',
+                  'School', 'Team', 'A', 'Pa', 
+                  'B', 'Pb', 'Total']"""
+
+table3 = table2.set_index('Unnamed: 1')
+
+"""if event = 'A'
+    column=1
+
+    for i in range(0, 34)"""
+
+print(table3)
+
+#print(table3.shape)
+
+print(table3.School)
+
+c = len(table3.School)
+print(c)
+
+
+
+#scoring = pandas.ExcelFile('/home/thomas/Documents/Code/Sailing_Techscore/Rank_Values.xls')
+
+score = pandas.read_excel(
+    '/home/thomas/Documents/Code/Sailing_Techscore/Rank_Values.xlsx', event)
+
+#score.columns = ['Place1', 'Score1', 'Na', 'Place2', 'Score2']
+
+print(score.Place)
+print(score.Place)
+
+print(list(score))
