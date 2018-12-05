613248df8d65a4fe3e78d26f8ec02dd053c1c53c
SanderMiller <42980999+SanderMiller@users.noreply.github.com>
2018-12-05 18:26:43 -0500

    New Ranking Sheet and scoreTable update

--- scoring_system.py
+++ scoring_system.py
@@ -13,7 +13,7 @@ engine='python'
 teams=pandas.read_html('https://docs.google.com/spreadsheets/d/1oDTZazH5M4ErQVKs3YtdrKEDRSgwDWMY_AM40SSpmjY/edit', header=0, index_col=0)
 linksSheet= pandas.read_html('https://docs.google.com/spreadsheets/d/1FG4fDoh5nt_hqBfj6m8Ndt0qsKWJYDmauJHY5tUu1HQ/edit#gid=0', header=0, index_col=0)
 
-scoringfile = pandas.ExcelFile('Rank_Values.xlsx')
+scoringfile = pandas.ExcelFile('Rank_Values2.xlsx')
 scoringTable_sc = pandas.read_excel(scoringfile, 'SC')
 scoringTable_sc_alt = pandas.read_excel(scoringfile, 'SC_alt')
 scoringTable_coed_a = pandas.read_excel(scoringfile, "Coed A")
@@ -23,37 +23,73 @@ scoringTable_w_sc = pandas.read_excel(scoringfile, 'W SC')
 scoringTable_w_a = pandas.read_excel(scoringfile, "Women's A")
 scoringTable_w_b = pandas.read_excel(scoringfile, "Women's B")
 
+"""events = Array[SC, SC_alt, Coed A, Coed B, Coed C, W SC, Women's A, Women's B]
+
+
+"""
+
 #print(scoringTable_coed_b)
 
 links=linksSheet[0]
 links.columns = ['regatta', 'link', 'type']
 num_regatta = len(links.link)
+
 #print(links.type[4])
 ##print(links)
 ##print(list (links))
 
-regattaType = []
 
-for i in range(2, num_regatta):
+for i in range(2, num_regatta+1):
+   
     link = links.link[i]
     #print(link)
     resultsTable = pandas.read_html(link, header=0, index_col=0)       
     results = resultsTable[0]
 
+    
+
     results.columns = [c.replace('Unnamed: 1', 'Place') for c in results.columns]
+
+    num_teams = len(results.Place)
+
+    scoringSheet = pandas.read_excel(scoringfile, links.type[i])
+    scoringTable = scoringSheet[scoringSheet.columns[(20-num_teams)]]
+    scoringTable.dropna(inplace=True)
+    scoringTable.columns = ['points']
+    #print(links.type[i])
+    #print(num_teams)
+    #print(20-num_teams)
+    print(scoringTable)
 
     tally = [results.Place, results.School]
     results2 = pandas.concat(tally, axis = 1)
 
+    #results['scoring'] = scoringTable
+
+    #results2.assign(Score=scoringTable)
+
+    # print(results2)
+    #print(results.School)
+    
+
+    
+   
+
+    #print(num_teams)
+
+    #print(scoringTable)
+    
+    
+
    
     #regattaType.append(links.type[i])
-    regattaType.append(links.type[i])
+    
 
 
     #print(regattaType)
 
 
-   # print(results2)
+    #print(results2)
 
     """results_final = results.set_index('Unnamed: 1')
 
@@ -63,7 +99,7 @@ for i in range(2, num_regatta):
 
     #print(results)
     
-print(regattaType)
+#print(regattaType)
 # for i in links.B:
 #     table = pandas.read_html('links.B[i]', header=0, index_col=0)
 #     print(table)
